0d0945d232b369e6ee22fa93a0f2c8479bbb9f44
NBC <joinshoaib@gmail.com>
2026-07-18 15:27:52 +0500

    fasdf

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.vertexline.tasbeeh_counter"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
 
     signingConfigs {
